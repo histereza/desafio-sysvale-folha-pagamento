f2d719c58fbb39c26c1d4c355ab81aaf04b289e0
Tereza Teles <86265429+histereza@users.noreply.github.com>
2022-07-19 14:04:20 -0300

    segundo-commit

--- funcaoFolha.js
+++ funcaoFolha.js
@@ -1,40 +1,50 @@
-//criando o objeto funcionário, para poder instanciá-lo mais de uma vez
+//criando a classe funcionário, para poder instanciá-la mais de uma vez
 class Funcionario {
   constructor(id, nome, salario) {
-    ;(this.id = id), (this.nome = nome), (this.salario = salario)
+    this.id = id
+    this.nome = nome
+    this.salario = salario
   }
 }
+//criando a classe folha, para poder instanciá-la mais de uma vez
+class Folha {
+  constructor(id, nome, salarioPago) {
+    this.id = id
+    this.nome = nome
+    this.salarioPago = salarioPago
+  }
+}
+
 //criando duas instâncias do objeto funcionario
 const TassioFreire = new Funcionario(1, 'Tássio Freire', 1200.35)
 const MariaDePaula = new Funcionario(2, 'Maria de Paula', 1250.48)
 
-totalSalarios = TassioFreire.salario + MariaDePaula.salario
-console.log(TassioFreire)
+//criando variáveis para identificar o total dos salarios
+var totalSalarios = TassioFreire.salario + MariaDePaula.salario
 
-function conferirFolha(id, nome, salario_pago) {
-  if (id == TassioFreire.id) nome == TassioFreire.nome
-  {
-    diferenca = TassioFreire.salario - salario_pago
-    if (diferenca == 0) {
-      console.log('Salario correto')
-    } else {
-      console.log('Salario incorreto. Diferença de R$', diferenca)
-    }
-  }
+//criando duas instancias do objeto folha
+var FolhaTassio = new Folha(1, 'Tássio Freire', 1260.35)
+var FolhaMaria = new Folha(2, 'Maria de Paula', 1250.48)
 
-  if (id == MariaDePaula.id) nome == MariaDePaula.nome
-  {
-    diferenca = MariaDePaula.salario - salario_pago
-    if (diferenca == 0) {
-      console.log('Salario correto')
-    } else {
-      console.log('Salario incorreto. Diferença de R$', diferenca)
-    }
-  }
+//calculando o total da Folha, a diferença entre o total da folha e o total dos salarios e a quantidade de salarios errados
+var totalFolha = FolhaTassio.salarioPago + FolhaMaria.salarioPago;
+var diferencaTotal = totalSalarios- totalFolha;
+var salariosErrados = 0;
+
+//comparando as instâncias dos salários base com as instancias da folha
+if (FolhaTassio.salarioPago !== TassioFreire.salario) {
+  salariosErrados = salariosErrados + 1
+  diferencaTassio = TassioFreire.salario - FolhaTassio.salarioPago
+  console.log("O salário de Tássio apresenta uma diferença de: R$", diferencaTassio)
 }
 
-tassioMes08 = conferirFolha(1, 'Tassio Freire', 1260.6) //Salario incorreto, diferença de R$60.25
-mariadpMes08 = conferirFolha(2, 'Maria de Paula', 1250.48)
+if (FolhaMaria.salarioPago !== MariaDePaula.salario) {
+  salariosErrados = salariosErrados + 1
+  diferencaMaria = MariaDePaula.salario - FolhaMaria.salarioPago
+  console.log("O salário de Maria de Paula apresenta uma diferença de: R$", diferencaMaria)
+}
 
-totalPagos = tassioMes08.salario_pago + mariadpMes08.salario_pago
-console.log('Total de diferenças: R$', totalPagos - totalSalarios)
+console.log("Número de salários incompatíveis com a folha: ", salariosErrados)
+console.log("A diferença total é de: R$", diferencaTotal)
+console.log("A diferença média é de: ", (diferencaTotal / salariosErrados))
+
